06e4918ba649fe6becb4fcfde9b814d8278b87c0
Ketevan12 <kirvk209@newschool.edu>
2023-10-16 15:32:21 -0400

    t

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to redirect to the map page
+function redirectToMapPage() {
+    window.location.href = 'map.html';
+}
+
+// Delay the redirection
+setTimeout(redirectToMapPage, 5000); // 5000 milliseconds (5 seconds)
